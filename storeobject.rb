9539636ff40e1fc61a8e4e4fdfafec267bf42496
Andrew Chalkley <andrew@chalkley.org>
2009-03-31 23:19:01 +0100

    Tidying up

--- storeobject.rb
+++ storeobject.rb
@@ -2,7 +2,7 @@ require "yaml"
 
 class StoreObject
   attr_accessor :name
-  
+
   def initialize(name)
     raise RuntimeError if self.instance_of? StoreObject
   end
@@ -11,23 +11,23 @@ class StoreObject
       files = File.join("#{store}", class_store, "*.yaml")
       Dir.glob(files).collect {|file| YAML.load(File.open(file))}
   end
-  
+
   def self.find(name)
     location = "#{store}/#{class_store}/#{name}.yaml"
     YAML.load(File.open(location)) if File.exists?(location)
   end
-  
+
   def self.class_store
     class_store = self.name.downcase
     class_store_location = store + "/" + self.name.downcase
     FileUtils.mkdir_p(class_store_location) if !File.exist?(class_store_location)
     class_store
   end
-  
+
   def class_store
     self.class.class_store
   end
-  
+
   def save
     raise RuntimeError if name.blank?
     File.open("#{store}/#{class_store}/#{name}.yaml", "w") {|file| YAML.dump(self,file) }
@@ -42,5 +42,5 @@ class StoreObject
   def store
     self.class.store
   end
-  
+
 end
